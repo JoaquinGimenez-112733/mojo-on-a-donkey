357b4e3d4465228bdffc7763a4b89698dac25c03
agodoy <godoyagustinnahuel93@gmail.com>
2026-01-31 13:44:20 -0300

    Nav Region y Nav Agent Minion

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,7 +1,39 @@
 extends CharacterBody3D
-class_name Enemy
+class_name Minion
+
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
+var target_pos: Vector3
+var has_target: bool = false
+@export var SPEED: float = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	target_pos = Vector3(0, 0, 0)
+	has_target = true
+	
 
 
-#func _physics_process(delta: float) -> void:
-	#velocity.z -= 10
-	#move_and_slide()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if has_target:
+		nav_agent.target_position = target_pos
+		var next_path_pos := nav_agent.get_next_path_position()
+		var direction := global_position.direction_to(next_path_pos)
+		velocity = direction.normalized() * SPEED
+		
+		if nav_agent.is_navigation_finished():
+			has_target = false
+			velocity = Vector3.ZERO
+			
+		## ROTACION EN MOVIMIENTO
+		var ROTATION_SPEED = 4
+		var target_angle := atan2(direction.x, direction.z)
+		rotation.y = lerp_angle(rotation.y, target_angle, delta * 10)
+		#var target_rotation := direction.signed_angle_to(Vector3.MODEL_FRONT, Vector3.DOWN)
+		#if abs(target_rotation - rotation.y) > deg_to_rad(60):
+			#ROTATION_SPEED = 20
+		#rotation.y = move_toward(rotation.y, target_rotation, delta * ROTATION_SPEED)
+		
+	move_and_slide()
+	print(nav_agent.is_navigation_finished())
+		
