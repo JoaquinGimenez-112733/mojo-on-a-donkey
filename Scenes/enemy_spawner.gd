2ff9fc5d089a2de4c53d21c698b50724f697256d
JoaquinGimenez-112733 <101760750+JoaquinGimenez-112733@users.noreply.github.com>
2026-02-01 11:23:18 -0300

    tuki

--- Scenes/enemy_spawner.gd
+++ Scenes/enemy_spawner.gd
@@ -1,12 +1,15 @@
 extends Node3D
 
 @export var enemy_scene: PackedScene
-@export var spawn_interval := randi_range(2, 5)
+@export var spawn_interval := randi_range(4, 7)
 @export var enemies_per_wave := 10
 @export var target_position: Vector3
 
 @onready var spawn_point: Marker3D = $Marker3D
 @onready var timer: Timer = $Timer
+
+const ENEMY_BAD_02 = preload("uid://ca6mv582j3hgh")
+const ENEMY_BAD_03 = preload("uid://bmmjmo4mmr3gq")
 
 var spawned := 0
 
@@ -19,8 +22,16 @@ func _spawn_enemy():
 	if spawned >= enemies_per_wave:
 		timer.stop()
 		return
-
-	var enemy = enemy_scene.instantiate()
+	var ranf = randf_range(0,1)
+	var packed : PackedScene
+	if ranf <= 0.33:
+		packed = ENEMY_BAD_02
+	elif ranf > 0.33 and ranf <= 0.66:
+		packed = ENEMY_BAD_03
+	else:
+		packed = ENEMY_BAD_02
+	#var enemy = enemy_scene.instantiate()
+	var enemy = packed.instantiate()
 	enemy.global_position = spawn_point.global_position
 
 	if enemy.has_method("set_target"):
